56706194082d97caede023b626c512e0ae9ddcde
web2integrators <gadgilchinmayee@gmail.com>
2018-03-27 16:17:42 -0400

    popularity and article similarity

--- Demo/dbConnect.py
+++ Demo/dbConnect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import MySQLdb
+import csv
 
 
 def getCurrencyNames():
@@ -14,6 +15,10 @@ def getCurrencyNames():
     currencies = cursor.fetchall()
     # disconnect from server
     db.close()
+    with open('list.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for c in currencies:
+            writer.writerow(c)
     return currencies
 
 
@@ -27,13 +32,48 @@ def getContent():
     return content
 
 
+def getPopularity():
+    db = MySQLdb.connect(host="cs336.ckksjtjg2jto.us-east-2.rds.amazonaws.com", port=3306,
+                         user="student", passwd="cs336student", db="CryptoNews")
+    cursor = db.cursor()
+    cursor.execute("select distinct(currency_name) from Value")
+    currencies = cursor.fetchall()
+    list = []
+    for currency in currencies:
+        currency = currency[0]
+        print currency
+        sql = "select count(distinct(content)) from cryptonews where date like '%2018-03-13%' and content like"
+        cursor.execute(sql + " '%%%s%%'" % (currency))
+        count = cursor.fetchall()
+        list.append([currency, count])
+    print list
+    db.close()
+
+
+def readCurrencies():
+    currencies = []
+    with open('list.csv', 'rb') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            currencies.append(row)
+    return currencies
+
+
 def getDomains(c_name):
     db = MySQLdb.connect(host="cs336.ckksjtjg2jto.us-east-2.rds.amazonaws.com", port=3306,
                          user="student", passwd="cs336student", db="CryptoNews")
     cursor = db.cursor()
-    cursor.execute("SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(link, '/', 3), '://', -1), '/', 1), '?', 1),'www.',-2) as domain, count(*) as frequency from cryptonews where content like %s group by domain order by frequency desc", ("%" + str(c_name) + "%",))
+    cursor.execute(
+        "SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(link, '/', 3), '://', -1), '/', 1), '?', 1),'www.',-2) as domain, count(*) as frequency from cryptonews where content like %s group by domain order by frequency desc",
+        ("%" + str(c_name) + "%",))
     domains = cursor.fetchall()
     # print domains
     db.close()
     return domains
 
+# getPopularity()
+
+# currencies = getCurrencyNames()
+# print currencies[0:5]
+# currencies = readCurrencies()
+# print currencies[0:5]
--- Demo/wordsFrequency.py
+++ Demo/wordsFrequency.py
@@ -1,4 +1,4 @@
-from dbConnect import getContent
+from dbConnect import getContent, getCurrencyNames
 from nltk.tokenize import word_tokenize  # to split sentences into words
 from nltk.corpus import stopwords  # to get a list of stopwords
 from collections import Counter  # to get words-frequency
@@ -17,7 +17,7 @@ def getWordsFreq():
         words.extend(tokens)
     words = map(lambda x: x.lower(), words)
 
-    # remove stopwords from our words list and also remove any word whose length is less than 3
+    # remove stopwords from our words list and also remove any word whose length is less than 4
     # stopwords are commonly occuring words like is, am, are, they, some, etc.
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words and len(word) > 3]
@@ -25,5 +25,6 @@ def getWordsFreq():
     words_freq = Counter(words).most_common()
 
     words_json = [{'text': str(word[0]), 'weight': int(word[1])} for word in words_freq]
-    # # json.dumps is used to convert json object i.e. dictionary or list into a string
+    # json.dumps is used to convert json object i.e. dictionary or list into a string
     return json.dumps(words_json)
+
